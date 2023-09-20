38de16213891c2dce9f24b7257d01889a264352a
Husein <husenfadhullah@gmail.com>
2023-09-20 11:55:03 +0800

    Hello

---
+++ ContohTipeData14.java
@@ -0,0 +1,8 @@
+public class ContohTipeData14 {
+    public static void main(String[] args) {
+    char golonganDarah = 'A';
+    short jumlahPenduduk = 1025;
+    float suhu = 60.50f;
+    double berat = 0.
+    }
+}
---
+++ ContohVariabel14.java
@@ -0,0 +1,19 @@
+public class ContohVariabel14 {
+    public static void main(String[] args) {
+
+
+String salahSatuHobiSayaAdalah = "Turu";
+boolean pandai = true;
+char jenisKelamin = 'L';
+byte umur = 18;
+double $ipk = 3.99, tinggi = 1.78;
+
+
+System.out.println("Hobi: " + salahSatuHobiSayaAdalah);
+System.out.println("Apakah pandai? " + pandai);
+System.out.println("Jenis kelamin: " + jenisKelamin);
+System.out.println("IPK: " + $ipk);
+System.out.println(String.format("Saya berumur %s dengan tinggi badan %s", umur, tinggi));
+
+        }
+}
