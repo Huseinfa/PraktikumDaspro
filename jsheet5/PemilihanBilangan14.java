3cf48f122a39dc063d1ca271c79e014f4a1c0fc2
Husein <husenfadhullah@gmail.com>
2023-10-04 11:25:26 +0800

    Hello

--- jsheet5/PemilihanBilangan14.java
+++ jsheet5/PemilihanBilangan14.java
@@ -1,20 +1,30 @@
 import java.util.Scanner;
 
     public class PemilihanBilangan14 {
-        public static void main(String[]  args){
-            Scanner sc = new Scanner(System.in);
-                System.out.print("Masukkan sebuah angka: ");
-                int angka = sc.nextInt();
+        // public static void main(String[]  args){
+        //     Scanner sc = new Scanner(System.in);
+        //         System.out.print("Masukkan sebuah angka: ");
+        //         int angka = sc.nextInt();
 
-            if (angka % 2 == 0)
-            {
-                System.out.println("Angka " + angka + " termasuk bilangan genap");
+        //     if (angka % 2 == 0)
+        //     {
+        //         System.out.println("Angka " + angka + " termasuk bilangan genap");
 
-            }
-            else
-            {
-                System.out.println("Angka " + angka + " termasuk bilangan ganjil");
-            }
+        //     }
+        //     else
+        //     {
+        //         System.out.println("Angka " + angka + " termasuk bilangan ganjil");
+        //     }
+        // }
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Masukkan sebuah angka: ");
+        int angka = input.nextInt();
+
+        {
+            String output = (angka % 2==0) ?   "Bilangan Genap" : "Bilangan Ganjil";
+            System.out.println(output);
         }
-    
+    }
 }
