86f5356c2c7d8773623d88804f2225e178cb9f14
Husein <husenfadhullah@gmail.com>
2023-10-11 11:56:38 +0800

    5

--- Praktikum06/Kabisat.java
+++ Praktikum06/Kabisat.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
             System.out.println("Masukkan Tahun: ");
             tahun = sc.nextInt();
 
-            String result = (tahun % 4 == 0 && tahun % 100 !=0) ? "Tahun Kabisat" : "Bukan Tahun Kabisat";
+            String result = (tahun % 4 == 0 && tahun % 100 !=0 || tahun % 400 == 0) ? "Tahun Kabisat" : "Bukan Tahun Kabisat";
             System.out.println(result);
     
     }
--- Praktikum06/KabisatNested14.java
+++ Praktikum06/KabisatNested14.java
@@ -8,12 +8,19 @@ import java.util.Scanner;
             System.out.println("Masukkan Tahun: ");
             tahun = sc.nextInt();
 
-            if ((tahun % 4) == 0) {
-                if ((tahun % 100) !=0){
-                System.out.println("Tahun Kabisat");
-                }else
-            System.out.println("Bukan Tahun Kabisat");
-            }
+            if ((tahun % 4 == 0)){
+                if ((tahun % 100 !=0)){
+                    System.out.println("Tahun Kabisat");
+
+                }else if ((tahun % 400 == 0)){
+                    System.out.println("Tahun Kabisat");
+
+                }else {
+                    System.out.println("Bukan Tahun Kabisat");
+                }
+            }else{
+                    System.out.println("Bukan Tahun Kabisat");
+                }
         
         }
     
