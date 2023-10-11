1820518db8c16485e1ca93bbe21d4aad2ebd34cc
Husein <husenfadhullah@gmail.com>
2023-10-11 10:49:07 +0800

    2

---
+++ jsheet4/Judul Algoritma = Bank14.c
@@ -0,0 +1,14 @@
+Judul Algoritma = Bank14
+
+Deklarasi:
+    double : tabunganAwal,tabunganAkhir,bunga;
+    double : persentaseBunga = 0.0002;
+    int : lamaMenabung;
+
+Deskripsi:
+1. print "masukkan tabunganAwal&lamaMenabung"
+2. read tabunganAwal&lamaMenabung
+3. bunga= persentaseBunga * tabunganAwal * lamaMenabung
+4. tabunganAkhir = tabunganAwal + bunga
+5. print bunga
+6. print tabunganAkhir
