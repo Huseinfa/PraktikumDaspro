3d0752137526d8f0dd9e2c79269b0a45410e0868
Huseinfa <94022973+Huseinfa@users.noreply.github.com>
2023-10-24 21:00:45 +0700

    2.3

--- Praktikum07/ForKelipatan14.java
+++ Praktikum07/ForKelipatan14.java
@@ -7,19 +7,22 @@ public class ForKelipatan14 {
         int bilangan, total = 0, counter = 0;
         double rata;
 
-        System.out.println("Masukkan bilangan 1-9: ");
+        System.out.println("Masukkan Bilangan 1-9: ");
         bilangan = sc.nextInt();
 
         for (int i = 0; i < 50; i++) {
             if (i % bilangan == 0) {
-                total += i;
+                total += 1;
                 counter++;
+
             }
         }
-        System.out.printf("Pada rentang 1-50 terdapat %d bilangan kelipatan %d dengan total %d", counter, bilangan, total);
+
+        System.out.printf("Pada rentang 1-50 terdapat %d bilangan kelipatan %d dengan total %d ", counter, bilangan, total);
 
 
         rata =(double) total / counter;
-        System.out.println("Rata-rata dari seluruh bilangan adalah: " + rata );
+        System.out.println("Rata-rata dari seluruh bilangan adalah: " + rata);
     }
+
 }
