6f520308ef60c30c6209042d248da84d87dc8f31
Huseinfa <94022973+Huseinfa@users.noreply.github.com>
2023-11-01 13:19:00 +0700

    search

---
+++ Praktikum09/LinearSearch14.java
@@ -0,0 +1,16 @@
+public class LinearSearch14 {
+    public static void main(String[] args) {
+        
+    int[] arrayint = {34, 18, 26, 48, 72, 20, 56,63};
+    int key = 20;
+    int hasil = 0;
+
+    for (int i = 0; i < arrayint.length; i++) {
+        if (arrayint[i] == key) {
+            hasil = i;
+            break;
+            }
+        }
+            System.out.println("Key ada dalam array pada posisi indeks ke-" + hasil);
+    }
+}
