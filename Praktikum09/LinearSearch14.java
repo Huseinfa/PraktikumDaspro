07c27c5b0b1f262aae4eba0b6d58291fa68c743f
Huseinfa <94022973+Huseinfa@users.noreply.github.com>
2023-11-01 14:06:32 +0700

    all result

--- Praktikum09/LinearSearch14.java
+++ Praktikum09/LinearSearch14.java
@@ -1,34 +1,38 @@
 import java.util.Scanner;
-    public class LinearSearch14 {
-    public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
 
-        
-    int[] arrayint;
-    int key,hasil = -1,p;
-    System.out.println("Masukkan jumlah elemen array: ");
-    p = sc.nextInt();
-    arrayint = new int[p];
-    
-    for (int i = 0; i < arrayint.length; i++) {
-        
-        System.out.println("Masukkan elemen array ke-" + i);
-        arrayint[i] = sc.nextInt();
-    }
+public class tes {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] arrayint;
+        int key, p;
+
+        System.out.println("Masukkan jumlah elemen array: ");
+        p = sc.nextInt();
+        arrayint = new int[p];
+
+        for (int i = 0; i < arrayint.length; i++) {
+            System.out.println("Masukkan elemen array ke-" + i);
+            arrayint[i] = sc.nextInt();
+        }
+
         System.out.println("Masukkan key yang ingin dicari: ");
         key = sc.nextInt();
 
-    for (int i = 0; i < arrayint.length; i++) {
-        if (key == arrayint[i]) {
-            hasil = i;
-            break;
+        int count = 0;
+
+        System.out.print("Key ada dalam array pada posisi indeks ke:");
+
+        for (int i = 0; i < arrayint.length; i++) {
+            if (key == arrayint[i]) {
+                System.out.print(" " + i);
+                count++;
             }
         }
-        
-        if (hasil != -1) {
-            System.out.println("Key ada dalam array pada posisi indeks ke-" + hasil);
-        } else {
+
+        if (count == 0) {
             System.out.println("Key tidak dapat ditemukan di dalam array");
         }
     }
 }
+
