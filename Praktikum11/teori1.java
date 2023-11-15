84d69a3d8a0e8b2370f8bf3925b602aaac6d66b9
Huseinfa <94022973+Huseinfa@users.noreply.github.com>
2023-11-15 11:14:56 +0700

    p1

---
+++ Praktikum11/teori1.java
@@ -0,0 +1,20 @@
+public class teori1 {
+    public static void main(String[] args) {
+
+
+    // for (int i = 0; i < 11; i++) {
+        // for (int j = 0; j <= i; j++) {
+        //     System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+        
+        for (int i = 0; i < 11; i++) {
+        for (int j = 10; j >= i; j--) {
+            System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+
