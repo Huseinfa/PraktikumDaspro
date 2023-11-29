978f1db753f69e0eed3d8bf736e3ca52dabadb26
Huseinfa <94022973+Huseinfa@users.noreply.github.com>
2023-11-29 13:19:10 +0700

    Percobaan 4

---
+++ Praktikum13/CobaArgs14.java
@@ -0,0 +1,12 @@
+public class CobaArgs14 {
+    public static void main(String[] args) {
+        if (args.length > 0) {
+            System.out.println("Hai " + args[0]);
+            System.out.println("Selamat datang di " + args[1]);
+            
+        } else {
+            System.out.println("Hai, Selamat Datang");
+            
+        }
+    }
+}
